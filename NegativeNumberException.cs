2d378826b8fbaa40fff7f7174d03c3201633a81f
Николай Ветошкин <nik88350759@yandex.ru>
2023-11-13 21:43:00 +0300

    Lesson6

---
+++ NegativeNumberException.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson6
+{
+
+
+    public class NegativeNumberException : Exception
+    {
+        //Создайте класс исключения NegativeNumberException,
+        //который будет использоваться при попытке выполнения операции,
+        //не поддерживающей отрицательные числа.
+
+        public NegativeNumberException()
+        { 
+            
+        
+        }
+        public NegativeNumberException(string massage) : base(massage)
+        {
+
+
+        }
+        public NegativeNumberException(string massage, Exception ex) : base(massage, ex)
+        {
+
+
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -4,7 +4,62 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //lesson1
+            /*
+            if (MyTryParse("4", out int num))
+            {
+                Console.WriteLine(num);
+            }
+            */
+            //lesson2
+            
+            try
+            {
+                ProcessNumber(-5);
+            }
+            catch (NegativeNumberException ex)
+            {
+                Console.WriteLine(ex);
+                Console.WriteLine();
+                Console.WriteLine();
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine(ex.InnerException.Message);
+                }
+            }
+            catch { } 
         }
+        static bool MyTryParse(string myString, out int num)
+        {
+            num = 0;
+            try
+            {          
+                num = Convert.ToInt32(myString);
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message);
+                return false;
+            }
+            return true;
+        }
+
+        //Создайте класс исключения NegativeNumberException,
+        //который будет использоваться при попытке выполнения операции,
+        //не поддерживающей отрицательные числа.
+       static void ProcessNumber(int numder)
+        {
+            if (numder < 0)
+            {
+                throw new NegativeNumberException("Ваше число отрицательное", new Exception("123"));
+            }
+            Console.WriteLine(numder);
+        }
+
+        //Добавьте обработку собственного исключения, а также кода,
+        //который обрабатывает вложенное исключение(InnerException)
+        //для предоставления более детальной информации.
+
+
     }
 }
